e588392a9a79e1519cd5fbe12fa81ccd20cbea55
Jon Edwards <jkedwards@me.com>
2015-05-22 21:03:46 -0700

    add index, retrieve, and put routes for projects, along with tests

--- test/plugins/endpoints/projects/retrieve.js
+++ test/plugins/endpoints/projects/retrieve.js
@@ -6,7 +6,7 @@ var Chai = require('chai');
 var Lab = require('lab');
 var Mongoose = require('mongoose');
 var Server = require('../../../../lib/server');
-var User = require('../../../../lib/models/user');
+var Project = require('../../../../lib/models/project');
 var Sinon = require('sinon');
 
 var lab = exports.lab = Lab.script();
@@ -21,7 +21,7 @@ var beforeEach = lab.beforeEach;
 
 var server;
 
-describe('PUT /users/{userId}/update', function(){
+describe('GET /projects/{projectId}', function(){
   before(function(done){
     Server.init(function(err, srvr){
       if(err){ throw err; }
@@ -43,29 +43,31 @@ describe('PUT /users/{userId}/update', function(){
     });
   });
 
-  it('should update a user', function(done){
-    server.inject({method: 'PUT', url: '/users/b00000000000000000000001/update', credentials: {_id: 'b00000000000000000000001'}, payload: {email: 'jessica@me.com'}}, function(response){
+  it('should get one project', function(done){
+    server.inject({method: 'GET', url: '/projects/c00000000000000000000001', credentials: {_id: 'b00000000000000000000001'}}, function(response){
       expect(response.statusCode).to.equal(200);
-      expect(response.result.email).to.equal('jessica@me.com');
-      expect(response.result.password).to.not.be.ok;
+      expect(response.result.name).to.equal('Edwards remodel');
       done();
     });
   });
-  it('should return a 400 because user to update was not found', function(done){
-    server.inject({method: 'PUT', url: '/users/nouserhere/update', credentials: {_id: 'b00000000000000000000001'}, payload: {email: 'jessica@me.com'}}, function(response){
-      expect(response.statusCode).to.equal(400);
-      done();
-    });
-  });
-  it('should return a 401 because no credentials were sent', function(done){
-    server.inject({method: 'PUT', url: '/users/b00000000000000000000001/update', payload: {email: 'jessica@me.com'}}, function(response){
+
+  it('should return a 401 due to missing credentials', function(done){
+    server.inject({method: 'GET', url: '/projects/c00000000000000000000001'}, function(response){
       expect(response.statusCode).to.equal(401);
       done();
     });
   });
+
+  it('should return a 400 due to missing record', function(done){
+    server.inject({method: 'GET', url: '/projects/notarealrecord', credentials: {_id: 'b00000000000000000000001'}}, function(response){
+      expect(response.statusCode).to.equal(400);
+      done();
+    });
+  });
+
   it('should encounter a db error', function(done){
-    var stub = Sinon.stub(User, 'findByIdAndUpdate').yields(new Error());
-    server.inject({method: 'PUT', url: '/users/b00000000000000000000001/update', credentials: {_id: 'b00000000000000000000001'}, payload: {email: 'jessedwards@me.com'}}, function(response){
+    var stub = Sinon.stub(Project, 'findById').yields(new Error());
+    server.inject({method: 'GET', url: '/projects/c00000000000000000000001', credentials: {_id: 'b00000000000000000000001'}}, function(response){
       expect(response.statusCode).to.equal(400);
       stub.restore();
       done();
